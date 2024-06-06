80ada29399a6afe85e92a6574b2a32ef8517b5f7
KUNGERMOoN <60297336+KUNGERMOoN@users.noreply.github.com>
2024-06-06 16:14:12 +0200

    Executable name changed
    - Changed the name of the generated executable
    - Added best score data to gitignore
    - other minor changes

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Snake++
+
 A simple Snake game written in Visual C++
 
 # Usage
-*(Note: the program is currently Windows-only)*
+
+_(Note: the program is currently Windows-only)_
 
 Go to [relases](https://github.com/KUNGERMOoN/Snake/releases) and download the exe for appropriate architecture.
-The program is self-contained and has no external dependencies (aside from some Windows insides).
+The program is self-contained and has no external dependencies (aside from some Windows APIs).
 You can run the program without Admin privileges, but it needs to be placed in a directory with write permissions (such as Documents)
--- Snake++.cpp
+++ Snake++.cpp
@@ -49,24 +49,9 @@ fstream Data;
 
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-#pragma region Colors
-#define color_black      0
-#define color_dark_blue  1
-#define color_dark_green 2
-#define color_light_blue 3
+//From: https://stackoverflow.com/a/67195569/21199309
 #define color_dark_red   4
-#define color_magenta    5
-#define color_orange     6
-#define color_light_gray 7
-#define color_gray       8
-#define color_blue       9
 #define color_green     10
-#define color_cyan      11
-#define color_red       12
-#define color_pink      13
-#define color_yellow    14
-#define color_white     15
-#pragma endregion
 
 #pragma region Animations
 
@@ -325,6 +310,7 @@ void GameOverAnimation() {
     }
 }
 
+//Ascii text art generator: https://patorjk.com/software/taag/
 void DrawMenuScreen() {
     if (!DrawnMenu) {
         SetColor(color_green);
